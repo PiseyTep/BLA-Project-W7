6e600fb0dae56582d094ecb5bbca7a5f193aa57a
Pisey Tep <piseytep26@gmail.com>
2025-03-28 01:41:59 +0700

    Bla-200-Start-code Week7

--- lib/model/ride/ride_pref.dart
+++ lib/model/ride/ride_pref.dart
@@ -2,7 +2,7 @@ import '../location/locations.dart';
 
 ///
 /// This model describes a ride preference.
-/// A ride preference consists of the selection of a departure + arrival + a date and a number of passenger
+/// A ride preference consists of the selection of a departure + arrival + a date and a number of passengers.
 ///
 class RidePreference {
   final Location departure;
@@ -10,11 +10,12 @@ class RidePreference {
   final Location arrival;
   final int requestedSeats;
 
-  const RidePreference(
-      {required this.departure,
-      required this.departureDate,
-      required this.arrival,
-      required this.requestedSeats});
+  const RidePreference({
+    required this.departure,
+    required this.departureDate,
+    required this.arrival,
+    required this.requestedSeats,
+  });
 
   @override
   String toString() {
@@ -22,5 +23,27 @@ class RidePreference {
         'departureDate: ${departureDate.toIso8601String()}, '
         'arrival: ${arrival.name}, '
         'requestedSeats: $requestedSeats)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true; // Check for reference equality
+    if (other is! RidePreference)
+      return false; // Check if the other object is of the same type
+
+    // Compare all properties for equality
+    return departure == other.departure &&
+        departureDate == other.departureDate &&
+        arrival == other.arrival &&
+        requestedSeats == other.requestedSeats;
+  }
+
+  @override
+  int get hashCode {
+    // Combine the hash codes of all properties
+    return departure.hashCode ^
+        departureDate.hashCode ^
+        arrival.hashCode ^
+        requestedSeats.hashCode;
   }
 }
